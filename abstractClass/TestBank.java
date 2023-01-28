6941b9bcf897232f74fb30aaf7c5b428e235aab8
Sunny Sinha <80207796+sunnysinha25@users.noreply.github.com>
2023-01-28 16:12:22 +0530

    Update TestBank.java

--- abstractClass/TestBank.java
+++ abstractClass/TestBank.java
@@ -5,16 +5,16 @@ public class TestBank {
 	public static void main(String[] args) 
 	{
 		Bank ob1 = new BankA();
-        ob1.setBalance(100);
-        System.out.println(ob1.getBalance());
+        	ob1.setBalance(100);
+        	System.out.println(ob1.getBalance());
 
-        Bank ob2 = new BankB();
-        ob2.setBalance(150);
-        System.out.println(ob2.getBalance());
+        	Bank ob2 = new BankB();
+        	ob2.setBalance(150);
+        	System.out.println(ob2.getBalance());
 
-        Bank ob3 = new BankC();
-        ob3.setBalance(200);
-        System.out.println(ob3.getBalance());
+        	Bank ob3 = new BankC();
+        	ob3.setBalance(200);
+        	System.out.println(ob3.getBalance());
 
 	}
 
